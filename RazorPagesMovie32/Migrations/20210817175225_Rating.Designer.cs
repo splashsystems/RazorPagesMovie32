69572a0400be283c15a8f73e56160b81e2ffda60
Scot <bscot.hagen@gmail.com>
2021-08-17 11:57:03 -0700

    Added a new field and Data Annotations

--- RazorPagesMovie32/Migrations/20210817175225_Rating.Designer.cs
+++ RazorPagesMovie32/Migrations/20210817175225_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie32.Data;
 
 namespace RazorPagesMovie32.Migrations
 {
     [DbContext(typeof(RazorPagesMovie32Context))]
-    partial class RazorPagesMovie32ContextModelSnapshot : ModelSnapshot
+    [Migration("20210817175225_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RazorPagesMovie32/Models/Movie.cs
+++ RazorPagesMovie32/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,26 @@ namespace RazorPagesMovie32.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string Rating { get; set; }
     }
 }
